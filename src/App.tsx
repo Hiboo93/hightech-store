e0825c6eaf02d216f579123f74925fe4068b4ead
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-03-28 03:30:26 +0100

    ✨ F05: Creating LatestPosts component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Banner from "./components/banner/Banner.tsx";
 import Features from "./components/features/Features.tsx";
+import LatestPosts from "./components/latestPosts/LatestPosts.tsx";
 import NavBar from "./components/navbar/NavBar.tsx";
 import ProductSlider from "./components/productSlider/ProductSlider.tsx";
-import { iphones, smartWatches } from "./data/index.ts";
+import { BlogPosts, iphones, smartWatches } from "./data/index.ts";
 import Header from "./header/Header.tsx";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <ProductSlider title="mobile & tablets" data={iphones} delay={7000} />
       <ProductSlider title="smart watches" data={smartWatches} delay={5000} />
       <Banner/>
+      <LatestPosts title="latest posts" posts={BlogPosts}/>
     </>
   );
 }
